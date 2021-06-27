ead79c82407b867f647b9e2726999740768acfee
Maya Weissman <mayaw10@gmail.com>
2021-06-27 12:01:25 +0300

    .

---
+++ src/models/pageModel.ts
@@ -0,0 +1,5 @@
+export class PageModel {
+  public constructor(
+    public is_video?: boolean,
+  ) { }
+}
--- src/models/productModel.ts
+++ src/models/productModel.ts
@@ -1,27 +1,51 @@
+import { PageModel } from "./pageModel";
+
 export class ProductModel {
   public constructor(
     public productId?: number,
-    public bucket?: string,
-    public campaignId?: number,
-    public clientId?: number,
+    public displayImgSrc?: string,
+    public displayVideoSrc?: string,
     public productTypeId?: number,
-    public successRates?: number,
-    public images?: {
-      img1?: string,
-      img2?: string,
-      img3?: string,
-      img4?: string,
-      img5?: string,
-      img6?: string,
-      img7?: string,
-      img8?: string,
-      img9?: string,
-      img10?: string,
-      img11?: string,
-      img12?: string,
-      img13?: string,
-      img14?: string,
-      img15?: string,
+    public insidePage?: {
+      section1?: {
+        imgSrc?: string,
+        videoSrc?: string
+      },
+      section2?: {
+        bgColor?: string,
+        imagesSize?: number,
+        images: {
+          img1?: string,
+          img2?: string,
+          img3?: string,
+          img4?: string,
+          img5?: string,
+          img6?: string,
+          img7?: string,
+          img8?: string,
+        }
+      }
+      section3?: {
+        bgColor?: string,
+        imagesSize?: number,
+        images: {
+          img1?: string,
+          img2?: string,
+          img3?: string,
+          img4?: string,
+          img5?: string,
+          img6?: string,
+          img7?: string,
+          img8?: string,
+        }
+      },
+      section4?: {
+        bgColor?: string,
+        bgImgSrc?: string,
+        imgSrc?: string,
+        videoSrc?: string,
+        display?: string
+      }
     }
   ) { }
 }
